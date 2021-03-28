dc5449cdacb7605c5c262b8fc6cc3fa112d44432
Kateryna Gordiienko <gordkat@gmail.com>
2021-03-28 23:31:40 +0200

    add markup to html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import './sass/main.scss';
-import './js/modal';
-import './js/pagination';
-import './js/search';
-import headerMarkup from './partials/header.html';
-import galleryMarkup from './partials/gallery.html';
 import footerMarkup from './partials/footer.html';
 
 const body = document.querySelector('body');
 
-body.insertAdjacentHTML('beforeend', headerMarkup);
-body.insertAdjacentHTML('beforeend', galleryMarkup);
 body.insertAdjacentHTML('beforeend', footerMarkup);
+
+import './js/modal';
+import './js/pagination';
+import './js/search';
