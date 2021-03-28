f851d9e483234a3303fa01903f98bd682065df24
Zoia Apostoloska <zoiaapostoloska@gmail.com>
2021-03-28 20:08:19 +0300

    creates markup for modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,1 +1,29 @@
-import modalMarkup from '../partials/modal.html';
+// import modalMarkup from '../partials/modal.html';
+// import modalMarkup from '../templates/modal-film-card.hbs';
+// import * as basicLightbox from 'basiclightbox';
+
+
+// modalRef.addEventListener('click', openModal)
+
+// function openModal(event) {
+//     if (event.target.localName === 'img') {
+//         function apiMovieCard (movieId) {
+//             const keyApi = 'be2bb7fd29eddf6e05cfa10ca2e7b19c';
+//             const baseUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${keyApi}`;
+//             return fetch(baseUrl)
+//               .then(res => res.json())
+//               .then(data => {
+//                 modalMarkup(data);
+//               })
+//               .then(data => {
+//                 basicLightbox.create(data).show();
+//              })
+//             .catch(err => console.log(err));
+//         }
+
+//         apiMovieCard(event.target.dataset.id);
+//     }
+// }
+
+
+  
