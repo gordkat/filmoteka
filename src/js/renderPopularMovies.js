bfae708027a947ad69520f486ea91b45e8027f31
Anna Shevchenko <uaskoa@gmail.com>
2021-03-29 23:35:31 +0300

    movie by search

--- src/js/renderPopularMovies.js
+++ src/js/renderPopularMovies.js
@@ -1,14 +1,33 @@
 import galleryTemplate from '../templates/film-card.hbs';
-import MovieApiService from './apiService'
-
+import MovieApiService from './apiService';
+import debounce from 'lodash';
 
 const movieApiServie = new MovieApiService();
 const galleryRef = document.querySelector('.gallery-list');
 
- export function renderMovieCard(results) {
+export function renderMovieCard(results) {
   galleryRef.insertAdjacentHTML('beforeend', galleryTemplate(results));
 }
 
+movieApiServie.getPopularMovies().then(renderMovieCard);
 
+// Пример для Марии. показывает, что отрисовывает по поиску
 
-movieApiServie.normalizedMovies().then(renderMovieCard);
+// const inputRef = document.querySelector('.input-text')
+
+// inputRef.addEventListener(
+//   'input',
+//   _.debounce(() => {
+//     onSearch();
+//   }, 1000),
+// );
+
+// function onSearch() {
+
+//   if (!inputRef.value) {
+//     return
+//   }
+//   movieApiServie.query = inputRef.value;
+//   galleryRef.innerHTML = '';
+//   movieApiServie.searchMovie().then(renderMovieCard);
+// }
