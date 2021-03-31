d1b810b100216cca6bbe4b70127fbba1785c40d4
Andrii Kondysiuk <kondysiuka@gmail.com>
2021-03-31 20:23:36 +0300

    add coments in header and myLibrary

--- src/js/myLibrary.js
+++ src/js/myLibrary.js
@@ -78,3 +78,12 @@ refs.btnHome.addEventListener('click', onHome);
 refs.btnMyLibrary.addEventListener('click', onMyLibrary);
 refs.btnWatched.addEventListener('click', onWatched);
 refs.btnQueue.addEventListener('click', onQueue);
+
+
+// let buttons = document.querySelectorAll('nav > button')
+// buttons.forEach(button =>{
+//     button.addEventListener('click',function(){
+//         buttons.forEach(btn=>btn.classList.remove('current'))
+//         this.classList.add('current')
+//     })
+// })
