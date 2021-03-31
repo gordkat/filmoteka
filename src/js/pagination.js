369e7ca388ec0c2228d4d0ded2a9f6df2f624622
koval-andrey <kovalandrey1234@gmail.com>
2021-03-31 20:17:30 +0300

    pag5
    отрисовка кнопок

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,183 +1,203 @@
-import MovieApiService from './apiService'
+
+import filmsCardTpl from "../templates/film-card.hbs";
+
+const listItems = [
+  "Film 1",
+  "Film 2",
+  "Film 3",
+  "Film 4",
+  "Film 5",
+  "Film 6",
+  "Film 7",
+  "Film 8",
+  "Film 9",
+  "Film 10",
+  "Film 11",
+  "Film 12",
+  "Film 13",
+  "Film 14",
+  "Film 15",
+  "Film 16",
+  "Film 17",
+  "Film 18",
+  "Film 19",
+  "Film 20",
+  "Film 21",
+  "Film 22",
+  "Film 23",
+  "Film 24",
+  "Film 25",
+  "Film 26",
+  "Film 27",
+  "Film 28",
+  "Film 29",
+  "Film 30",
+  "Film 31",
+  "Film 32",
+  "Film 33",
+  "Film 34",
+  "Film 35",
+  "Film 36",
+  "Film 37",
+  "Film 38",
+  "Film 39",
+  "Film 40",
+  "Film 41",
+  "Film 42",
+  "Film 43",
+  "Film 44",
+  "Film 45",
+  "Film 46",
+  "Film 47",
+  "Film 48",
+];
 
 
-const movieApiServie = new MovieApiService();
-const listElement = document.querySelector('.cards-container');
-const paginationElement = document.getElementById('pagination');
-const arrowLeft = document.querySelector('.arrow_left');
-const arrowRight = document.querySelector('.arrow_right');
-
+const listElement = document.querySelector(".list");
+const paginationElement = document.getElementById("pagination");
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
 let currentPage = 1;
-let pageCount;
 const pagesOnWindow = 5;
-let rows = 20;
-
-function resetCurrentPage() {
-  currentPage = 1;
-}
 
 
-// главная функция для рендера pagination. Callback - функция для работы с fetch (зависит от раздела, где рисуем pagination)
-export function renderPagination(totalPages, listItems, callback, searchQuery) {
-  paginationElement.innerHTML = '';
-  resetCurrentPage();
-  arrowLeft.removeEventListener('click', onArrowLeftClick);
-  arrowRight.removeEventListener('click', onArrowRightClick);
+let rows = 5;
+const allPageCount = Math.ceil(listItems.length / rows);
 
-  function setupPagination(items, wrapper, rowsPerPage) {
-    wrapper.innerHTML = '';
+function setupPagination(items, wrapper, rowsPerPage) {
+  wrapper.innerHTML = "";
 
-    pageCount = totalPages;
-    let maxLeftPage = currentPage - Math.floor(pagesOnWindow / 2);
-    let maxRightPage = currentPage + Math.floor(pagesOnWindow / 2);
+  let pageCount = Math.ceil(items.length / rowsPerPage);
+  let maxLeftPage = currentPage - Math.floor(pagesOnWindow / 2);
+  let maxRightPage = currentPage + Math.floor(pagesOnWindow / 2);
+
+  if (maxLeftPage < 1) {
+    maxLeftPage = 1;
+    maxRightPage = pagesOnWindow;
+  }
+
+  if (maxRightPage > allPageCount) {
+    maxLeftPage = allPageCount - (pagesOnWindow - 1);
 
     if (maxLeftPage < 1) {
       maxLeftPage = 1;
-      maxRightPage = pagesOnWindow;
     }
-
-    if (maxRightPage > totalPages) {
-      maxLeftPage = totalPages - (pagesOnWindow - 1);
-
-      if (maxLeftPage < 1) {
-        maxLeftPage = 1;
-      }
-      maxRightPage = totalPages;
-    }
-
-    for (let i = 1; i <= totalPages; i++) {
-      if (maxLeftPage !== 1 && i == 1) {
-        let btn = paginationButton(i, items);
-        wrapper.appendChild(btn);
-      }
-
-      if (maxRightPage !== totalPages && i == totalPages) {
-        let btn = paginationButton(i, items);
-        wrapper.appendChild(btn);
-      }
-
-      if (i >= maxLeftPage && i <= maxRightPage) {
-        let btn = paginationButton(i, items);
-        wrapper.appendChild(btn);
-      }
-
-      // добавляет троеточие в pagination в зависимости от текущей страницы и общего к-ва страниц
-      if (
-        totalPages >= 6 &&
-        i == 1 &&
-        currentPage !== 1 &&
-        currentPage !== 2 &&
-        currentPage !== 3
-      ) {
-        const threeDotsEl = addThreeDotsBlock();
-        wrapper.insertBefore(threeDotsEl, wrapper[wrapper.length - 2]);
-      }
-
-      if (
-        pageCount >= 7 &&
-        i == pageCount - 1 &&
-        currentPage !== pageCount &&
-        currentPage !== pageCount - 2 &&
-        currentPage !== pageCount - 1
-      ) {
-        const threeDotsEl = addThreeDotsBlock();
-        wrapper.insertBefore(threeDotsEl, wrapper[1]);
-      }
-    }
-
-    //placeholder.spinner.close();
-  }
-  // создает троеточия для pagination
-  function addThreeDotsBlock() {
-    const threeDots = document.createElement('div');
-    threeDots.classList.add('threeDots');
-    threeDots.innerText = '...';
-    return threeDots;
+    maxRightPage = allPageCount;
   }
 
-  function paginationButton(page, items) {
-    let button = document.createElement('button');
-    button.innerText = page;
-
-    if (currentPage == page) button.classList.add('active');
-
-    button.addEventListener('click', function () {
-      warningField.textContent = ``;
-      //placeholder.spinner.show();
-      //window.scrollTo({ top: 0, behavior: 'smooth' });
-      currentPage = page;
-      callback(listElement, currentPage, searchQuery);
-
-      let current_btn = document.querySelector('.pagenumbers button.active');
-      current_btn.classList.remove('active');
-
-      button.classList.add('active');
-      setupPagination(listItems, paginationElement, rows);
-      //hideExtremeButtons(totalPages);
-    });
-
-    return button;
-  }
-
-  // ф-кция для отслеживания кликов по стрелке влево
-  function onArrowLeftClick() {
-    if (currentPage > 1) {
-      //placeholder.spinner.show();
-      // window.scrollTo({ top: 0, behavior: 'smooth' });
-      currentPage--;
-      setupPagination(listItems, paginationElement, rows);
-      callback(listElement, currentPage, searchQuery);
+  for (let i = 1; i <= allPageCount; i++) {
+    if (maxLeftPage !== 1 && i == 1) {
+      let btn = paginationButton(i, items);
+      wrapper.appendChild(btn);
     }
 
-    disableArrowBtn(totalPages);
-    //hideExtremeButtons(totalPages);
-  }
-
-  // ф-кция для отслеживания кликов по стрелке вправо
-  function onArrowRightClick() {
-    if (currentPage < totalPages) {
-      //placeholder.spinner.show();
-      //window.scrollTo({ top: 0, behavior: 'smooth' });
-      currentPage++;
-      setupPagination(listItems, paginationElement, rows);
-      callback(listElement, currentPage, searchQuery);
+    if (maxRightPage !== allPageCount && i == allPageCount) {
+      let btn = paginationButton(i, items);
+      wrapper.appendChild(btn);
     }
-    disableArrowBtn(totalPages);
-    //hideExtremeButtons(totalPages);
-  }
 
-  setupPagination(listItems, paginationElement, rows);
-  arrowLeft.onclick = onArrowLeftClick;
-  arrowRight.onclick = onArrowRightClick;
+    if (i >= maxLeftPage && i <= maxRightPage) {
+      let btn = paginationButton(i, items);
+      wrapper.appendChild(btn);
+    }
 
-  //hideExtremeButtons(totalPages);
-  disableArrowBtn(totalPages);
-}
+    if (
+      allPageCount >= 6 &&
+      i == 1 &&
+      currentPage !== 1 &&
+      currentPage !== 2 &&
+      currentPage !== 3
+    ) {
+      const threeDotsEl = addThreeDotsBlock();
+      wrapper.insertBefore(threeDotsEl, wrapper[wrapper.length - 2]);
+    }
 
-// прячет первую и последнюю страницу по бокам для мобильных гаджетов с маленьким экраном
-
-paginationElement.addEventListener('click', disableArrowBtnAfterPageClick);
-
-function disableArrowBtnAfterPageClick(e) {
-  if (e.target.tagName != 'BUTTON') {
-    return;
-  } else {
-    disableArrowBtn(pageCount);
+    if (
+      pageCount >= 7 &&
+      i == pageCount - 1 &&
+      currentPage !== pageCount &&
+      currentPage !== pageCount - 2 &&
+      currentPage !== pageCount - 1
+    ) {
+      const threeDotsEl = addThreeDotsBlock();
+      wrapper.insertBefore(threeDotsEl, wrapper[1]);
+    }
   }
 }
 
-// делает неактивными кнопки-стрелки на первой и последней  странице
-function disableArrowBtn(totalPages) {
-  if (currentPage === 1) {
-    arrowLeft.classList.add('disabled-arrow');
-  } else {
-    arrowLeft.classList.remove('disabled-arrow');
-  }
+function addThreeDotsBlock() {
+  const threeDots = document.createElement("div");
+  threeDots.classList.add("threeDots");
+  threeDots.innerText = "...";
+  return threeDots;
+}
 
-  if (currentPage === totalPages) {
-    arrowRight.classList.add('disabled-arrow');
-  } else {
-    arrowRight.classList.remove('disabled-arrow');
+function paginationButton(page, items) {
+  let button = document.createElement("button");
+  button.innerText = page;
+
+  if (currentPage == page) button.classList.add("active");
+
+  button.addEventListener("click", function () {
+    currentPage = page;
+    // displayList(items, listElement, rows, currentPage);
+
+    let current_btn = document.querySelector(".pagenumbers button.active");
+    current_btn.classList.remove("active");
+
+    button.classList.add("active");
+    setupPagination(listItems, paginationElement, rows);
+  });
+
+  return button;
+}
+
+function onArrowLeftClick() {
+  if (currentPage > 1) {
+    currentPage--;
+    setupPagination(listItems, paginationElement, rows);
+    // displayList(listItems, listElement, rows, currentPage);
   }
 }
 
+function onArrowRightClick() {
+  if (currentPage < allPageCount) {
+    currentPage++;
+    setupPagination(listItems, paginationElement, rows);
+    // displayList(listItems, listElement, rows, currentPage);
+  }
+}
+
+setupPagination(listItems, paginationElement, rows);
+arrowLeft.addEventListener("click", onArrowLeftClick);
+arrowRight.addEventListener("click", onArrowRightClick);
+
+/*
+функция для добавления контента на страницу
+параментры:
+items = массив с данными для отрисовки
+wrapper = родитель, куда полетит рендер
+rowsPerPage = количество элементов на странице
+page = текущая страница выбранная в pagination
+*/
+
+function displayList(items, wrapper, rowsPerPage, page) {
+  wrapper.innerHTML = "";
+  page--;
+
+  let start = rowsPerPage * page;
+  let end = start + rowsPerPage;
+
+  
+  let paginatedItems = items.slice(start, end);
+
+  for (let i = 0; i < paginatedItems.length; i++) {
+    let item = paginatedItems[i];
+
+    let itemElement = document.createElement("div");
+    itemElement.classList.add("item");
+    itemElement.innerText = item;
+
+    wrapper.appendChild(itemElement);
+  }
+}
