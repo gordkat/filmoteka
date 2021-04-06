4695ee51df471777583553836c0f0eb631d05ff3
koval-andrey <kovalandrey1234@gmail.com>
2021-04-06 19:18:55 +0300

    pag10
    рабочие стрелки и стиль под мобилку

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -179,7 +179,7 @@ function renderPagination(totalPages, listItems, callback) {
     if (currentPage > 1) {
       currentPage--;
       setupPagination(listItems, paginationElement, rows);
-      callback(listElement, currentPage, searchQuery);
+      callback(listElement, currentPage);
     }
   }
 
@@ -187,7 +187,7 @@ function renderPagination(totalPages, listItems, callback) {
     if (currentPage < totalPages) {
       currentPage++;
       setupPagination(listItems, paginationElement, rows);
-      callback(listElement, currentPage, searchQuery);
+      callback(listElement, currentPage);
     }
   }
 
