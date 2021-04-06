67c015c5e92cfd585efdd006daffdabe6297d4c6
Lubasia <lubasia.oasis@gmail.com>
2021-04-06 13:21:59 +0300

    Темная тема done. + поправлены свг

--- src/js/modalTeam.js
+++ src/js/modalTeam.js
@@ -12,129 +12,200 @@ import social from '../images/sprite.svg';
 
 const markup = `<ul class="team__wrapper">
 
-<li class="team__lead list">
-<img src="${Kateryna}" alt="Kateryna" class="team__image">
-    <p class="team__name">Kateryna</p>
-    <p class="team__role">Team Lead / Developer </p>
-  <ul class="team__git">
-  <a class="" href="https://github.com/gordkat" target="_blank" ><svg class="logo__icon" width="16" height="16">
-  <use href="${social}#github"></use>
-  </svg></a>
-  <a class="" href="https://linkedin.com" target="_blank" ><svg class="logo__icon" width="16" height="16">
-  <use href="${social}#linkedin"></use>
-</svg></a>
-</ul>
-</li>
+    <li class="team__lead list">
+        <img src="${Kateryna}" alt="Kateryna" class="team__image">
+        <p class="team__name">Kateryna</p>
+        <p class="team__role">Team Lead / Developer </p>
+        <ul class="team__git">
+            <a class="" href="https://github.com/gordkat" target="_blank"><svg 
+            type="image/svg+xml"
+            class="logo__icon" 
+            width="16" height="16">
+                    <use 
+                    type="image/svg+xml" 
+                    href="${social}#github"></use>
+                </svg></a>
+            <a class="" href="https://linkedin.com" target="_blank"><svg 
+            type="image/svg+xml"
+            class="logo__icon" 
+            width="16" height="16">
+                    <use type="image/svg+xml" 
+                    type="image/svg+xml" 
+                    href="${social}#linkedin"></use>
+                </svg></a>
+        </ul>
+    </li>
 
-<li class="team__card">
-    <img src="${Zoya}" alt="Zoya" class="team__image">
-    <p class="team__name">Zoya</p>
-    <p class="team__role">Scrum/Developer</p>
+    <li class="team__card">
+        <img src="${Zoya}" alt="Zoya" class="team__image">
+        <p class="team__name">Zoya</p>
+        <p class="team__role">Scrum/Developer</p>
 
-     <ul class="team__git">
-    <a class="" href="https://github.com/apostoloska-z" target="_blank" ><svg class="logo__icon" width="16" height="16">
-      <use href="${social}#github"></use>
-    </svg></a>
-    <a class="" href="https://linkedin.com" target="_blank" ><svg class="logo__icon" width="16" height="16">
-      <use href="${social}#linkedin"></use>
-    </svg></a>
-    </ul>
-</li>
+        <ul class="team__git">
+            <a class="" href="https://github.com/apostoloska-z" target="_blank"><svg 
+            type="image/svg+xml"
+             class="logo__icon" 
+             width="16" height="16">
+                    <use type="image/svg+xml" 
+                    href="${social}#github"></use>
+                </svg></a>
+            <a class="" href="https://linkedin.com" target="_blank"><svg 
+            type="image/svg+xml"
+            class="logo__icon" 
+            width="16" height="16">
+                    <use type="image/svg+xml" 
+                    href="${social}#linkedin"></use>
+                </svg></a>
+        </ul>
+    </li>
 
-<li class="team__card">
-    <img src="${Anna}" alt="Anna" class="team__image">
-    <p class="team__name">Anna</p>
-    <p class="team__role">Developer</p>
+    <li class="team__card">
+        <img src="${Anna}" alt="Anna" class="team__image">
+        <p class="team__name">Anna</p>
+        <p class="team__role">Developer</p>
 
-    <ul class="team__git">
-    <a class="" href="https://github.com/Uaskoa" target="_blank" ><svg class="logo__icon" width="16" height="16">
-      <use href="${social}#github"></use>
-    </svg></a>
-    <a class="" href="https://linkedin.com" target="_blank" ><svg class="logo__icon" width="16" height="16">
-      <use href="${social}#linkedin"></use>
-    </svg></a>
-    </ul>
-</li>
+        <ul class="team__git">
+            <a class="" href="https://github.com/Uaskoa" target="_blank"><svg 
+            type="image/svg+xml"
+            class="logo__icon" 
+            width="16" height="16">
+                    <use type="image/svg+xml" 
+                    href="${social}#github"></use>
+                </svg></a>
+            <a class="" href="https://linkedin.com" target="_blank"><svg type="image/svg+xml"
+            class="logo__icon" 
+            width="16" height="16">
+                    <use type="image/svg+xml" 
+                    href="${social}#linkedin"></use>
+                </svg></a>
+        </ul>
+    </li>
 
-<li class="team__card">
-    <img src="${Anatoliy}" alt="Anatoliy" class="team__image">
-    <p class="team__name">Anatoliy</p>
-    <p class="team__role">Developer</p>
-    <ul class="team__git">
-    <a class="" href="https://github.com/AnatoliiYarovyi" target="_blank" ><svg class="logo__icon" width="16" height="16">
-      <use href="${social}#github"></use>
-    </svg></a>
-    <a class="" href="https://linkedin.com" target="_blank" ><svg class="logo__icon" width="16" height="16">
-      <use href="${social}#linkedin"></use>
-    </svg></a>
-    </ul>
-</li>
+    <li class="team__card">
+        <img src="${Anatoliy}" alt="Anatoliy" class="team__image">
+        <p class="team__name">Anatoliy</p>
+        <p class="team__role">Developer</p>
+        <ul class="team__git">
+            <a class="" href="https://github.com/AnatoliiYarovyi" target="_blank"><svg 
+            type="image/svg+xml"
+            class="logo__icon" 
+            width="16" height="16">
+                    <use type="image/svg+xml" 
+                    href="${social}#github"></use>
+                </svg></a>
+            <a class="" href="https://linkedin.com" target="_blank"><svg type="image/svg+xml"
+            class="logo__icon" 
+            width="16" height="16">
+                    <use type="image/svg+xml" 
+                    href="${social}#linkedin"></use>
+                </svg></a>
+        </ul>
+    </li>
 
-<li class="team__card">
-    <img src="${Liubov}" alt="Liubov" class="team__image">
-    <p class="team__name">Liubov</p>
-    <p class="team__role">Developer</p>
-    <ul class="team__git">
-    <a class="" href="https://github.com/Lubasia" target="_blank" ><svg class="logo__icon" width="16" height="16">
-      <use href="${social}#github"></use>
-    </svg></a>
-    <a class="" href="https://linkedin.com" target="_blank" ><svg class="logo__icon" width="16" height="16">
-      <use href="${social}#linkedin"></use>
-    </svg></a>
-    </ul>
-</li>
-<li class="team__card">
-    <img src="${Maria}" alt="Mariya" class="team__image">
-    <p class="team__name">Mariya</p>
-    <p class="team__role">Developer</p>
-    <ul class="team__git">
-    <a class="" href="https://github.com/Narmony" target="_blank" ><svg class="logo__icon" width="16" height="16">
-      <use href="${social}#github"></use>
-    </svg></a>
-    <a class="" href="https://linkedin.com" target="_blank" ><svg class="logo__icon" width="16" height="16">
-      <use href="${social}#linkedin"></use>
-    </svg></a>
-    </ul>
-</li>
-<li class="team__card">
-    <img src="${Andrey}" alt="Andrey" class="team__image">
-    <p class="team__name">Andrey</p>
-    <p class="team__role">Developer</p>
-    <ul class="team__git">
-    <a class="" href="https://github.com/koval-andrey" target="_blank" ><svg class="logo__icon" width="16" height="16">
-      <use href="${social}#github"></use>
-    </svg></a>
-    <a class="" href="https://linkedin.com" target="_blank" ><svg class="logo__icon" width="16" height="16">
-      <use href="${social}#linkedin"></use>
-    </svg></a>
-    </ul>
-</li>
-<li class="team__card">
-    <img src="${Elena}" alt="Elena" class="team__image">
-    <p class="team__name">Elena</p>
-    <p class="team__role">Developer</p>
-    <ul class="team__git">
-    <a class="" href="https://github.com/el-ki" target="_blank" ><svg class="logo__icon" width="16" height="16">
-      <use href="${social}#github"></use>
-    </svg></a>
-    <a class="" href="https://linkedin.com" target="_blank" ><svg class="logo__icon" width="16" height="16">
-      <use href="${social}#linkedin"></use>
-    </svg></a>
-    </ul>
-</li>
-<li class="team__card">
-    <img src="${Andrii}" alt="Andrii" class="team__image">
-    <p class="team__name">Andrii</p>
-    <p class="team__role">Developer</p>
-    <ul class="team__git">
-    <a class="" href="https://github.com/Kondes" target="_blank" ><svg class="logo__icon" width="16" height="16">
-      <use href="${social}#github"></use>
-    </svg></a>
-    <a class="" href="https://linkedin.com" target="_blank" ><svg class="logo__icon" width="16" height="16">
-      <use href="${social}#linkedin"></use>
-    </svg></a>
-    </ul>
-</li></ul>`;
+    <li class="team__card">
+        <img src="${Liubov}" alt="Liubov" class="team__image">
+        <p class="team__name">Liubov</p>
+        <p class="team__role">Developer</p>
+        <ul class="team__git">
+            <a class="" href="https://github.com/Lubasia" target="_blank"><svg 
+            type="image/svg+xml"
+            class="logo__icon" 
+            width="16" height="16">
+                    <use type="image/svg+xml"
+                    href="${social}#github"></use>
+                </svg></a>
+            <a class="" href="https://linkedin.com" target="_blank"><svg 
+            type="image/svg+xml"
+            class="logo__icon" 
+            width="16" height="16">
+                    <use type="image/svg+xml"
+                    href="${social}#linkedin"></use>
+                </svg></a>
+        </ul>
+    </li>
+    <li class="team__card">
+        <img src="${Maria}" alt="Mariya" class="team__image">
+        <p class="team__name">Mariya</p>
+        <p class="team__role">Developer</p>
+        <ul class="team__git">
+            <a class="" href="https://github.com/Narmony" target="_blank"><svg 
+            type="image/svg+xml"
+            class="logo__icon" 
+            width="16" height="16">
+                    <use type="image/svg+xml"
+                    href="${social}#github"></use>
+                </svg></a>
+            <a class="" href="https://linkedin.com" target="_blank"><svg 
+            type="image/svg+xml"
+            class="logo__icon" 
+            width="16" height="16">
+                    <use type="image/svg+xml"
+                    href="${social}#linkedin"></use>
+                </svg></a>
+        </ul>
+    </li>
+    <li class="team__card">
+        <img src="${Andrey}" alt="Andrey" class="team__image">
+        <p class="team__name">Andrey</p>
+        <p class="team__role">Developer</p>
+        <ul class="team__git">
+            <a class="" href="https://github.com/koval-andrey" target="_blank"><svg 
+            type="image/svg+xml"
+            class="logo__icon" width="16" height="16">
+                    <use type="image/svg+xml"
+                    href="${social}#github"></use>
+                </svg></a>
+            <a class="" href="https://linkedin.com" target="_blank"><svg 
+            type="image/svg+xml"
+            class="logo__icon" 
+            width="16" height="16">
+                    <use type="image/svg+xml"
+                    href="${social}#linkedin"></use>
+                </svg></a>
+        </ul>
+    </li>
+    <li class="team__card">
+        <img src="${Elena}" alt="Elena" class="team__image">
+        <p class="team__name">Elena</p>
+        <p class="team__role">Developer</p>
+        <ul class="team__git">
+            <a class="" href="https://github.com/el-ki" target="_blank"><svg 
+            type="image/svg+xml"
+            class="logo__icon" 
+            width="16" height="16">
+                    <use href="${social}#github"></use>
+                </svg></a>
+            <a class="" href="https://linkedin.com" target="_blank"><svg 
+            type="image/svg+xml"
+            class="logo__icon" 
+            width="16" height="16">
+                    <use type="image/svg+xml"
+                    href="${social}#linkedin"></use>
+                </svg></a>
+        </ul>
+    </li>
+    <li class="team__card">
+        <img src="${Andrii}" alt="Andrii" class="team__image">
+        <p class="team__name">Andrii</p>
+        <p class="team__role">Developer</p>
+        <ul class="team__git">
+            <a class="" href="https://github.com/Kondes" target="_blank"><svg 
+            type="image/svg+xml"
+            class="logo__icon" 
+            width="16" height="16">
+                    <use type="image/svg+xml"
+                    href="${social}#github"></use>
+                </svg></a>
+            <a class="" href="https://linkedin.com" target="_blank"><svg 
+            type="image/svg+xml"
+            class="logo__icon" 
+            width="16" height="16">
+                    <use type="image/svg+xml"
+                    href="${social}#linkedin"></use>
+                </svg></a>
+        </ul>
+    </li>
+</ul>`;
 
 const container = document.querySelector('.js-team__modal');
 
